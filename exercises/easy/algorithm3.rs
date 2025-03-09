32ea820838df57d0c70f3b7a2d7f014e9dd8e563
qymlyl <mqy224512@foxmail.com>
2025-03-09 12:18:34 +0800

    feat: add sort and bst

--- exercises/easy/algorithm3.rs
+++ exercises/easy/algorithm3.rs
@@ -1,13 +1,23 @@
-/*
-	sort
-	This problem requires you to implement a sorting algorithm
-	you can use bubble sorting, insertion sorting, heap sorting, etc.
-*/
 
 
-fn sort<T>(array: &mut [T]){
-	//TODO
+fn sort<T>(array: &mut [T]) where T: PartialOrd {
+    let n = array.len();
+    for i in 0..n {
+        let mut swapped = false;
+        for j in 0..n-i-1 {
+            if array[j] > array[j+1] {
+                array.swap(j, j+1);
+                swapped=true;
+            }
+        }
+
+        // 没有触发比较，说明已经是有序的了
+        if !swapped  {
+            break
+        }
+    }
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -18,13 +28,13 @@ mod tests {
         sort(&mut vec);
         assert_eq!(vec, vec![19, 37, 46, 57, 64, 73, 75, 91]);
     }
-	#[test]
+    #[test]
     fn test_sort_2() {
         let mut vec = vec![1];
         sort(&mut vec);
         assert_eq!(vec, vec![1]);
     }
-	#[test]
+    #[test]
     fn test_sort_3() {
         let mut vec = vec![99, 88, 77, 66, 55, 44, 33, 22, 11];
         sort(&mut vec);
--- exercises/easy/algorithm4.rs
+++ exercises/easy/algorithm4.rs
@@ -4,14 +4,14 @@
 */
 
 
-use std::cmp::Ordering;
-use std::fmt::Debug;
+use std::fmt::Pointer;
 
 
+// 子树左边的叶子小于子树的根和右边的叶子
 #[derive(Debug)]
 struct TreeNode<T>
 where
-    T: Ord,
+    T: Ord + std::fmt::Display,
 {
     value: T,
     left: Option<Box<TreeNode<T>>>,
@@ -21,15 +21,16 @@ where
 #[derive(Debug)]
 struct BinarySearchTree<T>
 where
-    T: Ord,
+    T: Ord + std::fmt::Display,
 {
     root: Option<Box<TreeNode<T>>>,
 }
 
 impl<T> TreeNode<T>
 where
-    T: Ord,
+    T: Ord + std::fmt::Display,
 {
+    // 构造函数
     fn new(value: T) -> Self {
         TreeNode {
             value,
@@ -41,35 +42,87 @@ where
 
 impl<T> BinarySearchTree<T>
 where
-    T: Ord,
+    T: Ord + std::fmt::Display,
 {
-
+    // 构造函数
     fn new() -> Self {
         BinarySearchTree { root: None }
     }
 
-    // Insert a value into the BST
+    // 向BST中插入一个值
     fn insert(&mut self, value: T) {
-        //TODO
+        match self.root {
+            Some(ref mut root) => root.insert(value),
+            None => self.root = Some(Box::new(TreeNode::new(value))),
+        }
     }
 
-    // Search for a value in the BST
     fn search(&self, value: T) -> bool {
-        //TODO
-        true
+        self.root.as_ref().map_or(false, |root| root.search(value))
+    }
+
+    fn in_order_traversal(&self) {
+        if let Some(ref root) = self.root {
+            root.in_order_traversal()
+        }
     }
 }
 
 impl<T> TreeNode<T>
 where
-    T: Ord,
+    T: Ord + std::fmt::Display,
 {
-    // Insert a node into the tree
     fn insert(&mut self, value: T) {
-        //TODO
+        self.insert_node(TreeNode::new(value))
+    }
+
+    fn insert_node(&mut self, new_node: TreeNode<T>) {
+        if new_node.value < self.value {
+            // 小于当前value，将值插入左子树
+            match self.left {
+                Some(ref mut left) => left.insert_node(new_node),
+                None => self.left = Some(Box::new(new_node)),
+            }
+        } else if new_node.value > self.value {
+            // 大于当前值，插入右子树
+            match self.right {
+                Some(ref mut right) => right.insert_node(new_node),
+                None => self.right = Some(Box::new(new_node)),
+            }
+        }
+
+        // 如果值相同，则不做任何处理
+    }
+
+    // 查询方法
+    fn search(&self, value: T) -> bool {
+        if value == self.value {
+            true
+        } else if value < self.value {
+            // 左子树查询
+            self.left.as_ref().map_or(false, |left| left.search(value))
+        } else {
+            // 右子树查询
+            self.right
+                .as_ref()
+                .map_or(false, |right| right.search(value))
+        }
+    }
+
+    // 中序遍历打印树中的元素
+    fn in_order_traversal(&self) {
+        if let Some(ref left) = self.left {
+            left.in_order_traversal();
+        }
+
+        // 打印
+        println!("{}", self.value);
+
+        if let Some(ref right) = self.right {
+            right.in_order_traversal();
+        }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -79,24 +132,22 @@ mod tests {
     fn test_insert_and_search() {
         let mut bst = BinarySearchTree::new();
 
-        
         assert_eq!(bst.search(1), false);
 
-        
         bst.insert(5);
         bst.insert(3);
         bst.insert(7);
         bst.insert(2);
         bst.insert(4);
 
-        
+        bst.in_order_traversal();
+
         assert_eq!(bst.search(5), true);
         assert_eq!(bst.search(3), true);
         assert_eq!(bst.search(7), true);
         assert_eq!(bst.search(2), true);
         assert_eq!(bst.search(4), true);
 
-        
         assert_eq!(bst.search(1), false);
         assert_eq!(bst.search(6), false);
     }
@@ -105,22 +156,17 @@ mod tests {
     fn test_insert_duplicate() {
         let mut bst = BinarySearchTree::new();
 
-        
         bst.insert(1);
         bst.insert(1);
 
-        
         assert_eq!(bst.search(1), true);
 
-        
         match bst.root {
             Some(ref node) => {
                 assert!(node.left.is_none());
                 assert!(node.right.is_none());
-            },
+            }
             None => panic!("Root should not be None after insertion"),
         }
     }
-}    
-
-
+}
